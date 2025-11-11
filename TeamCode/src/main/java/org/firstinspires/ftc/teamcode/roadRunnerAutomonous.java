317f8f9bfa84176c1e1745d87d22f78c3f3851d8
YEtestacc <478967@bsd48.org>
2025-11-11 12:03:15 -0800

    Odo changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadRunnerAutomonous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadRunnerAutomonous.java
@@ -1,12 +1,36 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 @Config
 @Autonomous
 public class roadRunnerAutomonous extends LinearOpMode {
+    @Override
     public void runOpMode() {
+        Pose2d initialPose = new Pose2d(-51, 49.5, Math.toRadians(307));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+
+        TrajectoryActionBuilder action = drive.actionBuilder(initialPose)
+                ;
+
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        Actions.runBlocking(
+                new SequentialAction(
+                        new SleepAction(10),
+                        action.build()
+                )
+        );
     }
 }
