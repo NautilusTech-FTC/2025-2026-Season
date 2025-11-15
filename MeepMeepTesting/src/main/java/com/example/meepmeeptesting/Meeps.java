f6f027e1fe61eeb165d9fde2f92f306e37375d45
YEtestacc <478967@bsd48.org>
2025-11-14 16:57:20 -0800

    Added Meeps

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/Meeps.java
@@ -0,0 +1,65 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class Meeps {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity redFar = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        RoadRunnerBotEntity blueFar = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        RoadRunnerBotEntity redClose = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        RoadRunnerBotEntity blueClose = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        redFar.runAction(redFar.getDrive().actionBuilder(new Pose2d(62, -15, Math.PI))
+                .lineToX(38)
+                .build());
+
+        blueFar.runAction(blueFar.getDrive().actionBuilder(new Pose2d(62, 15, Math.PI))
+                .lineToX(38)
+                .build());
+
+        redClose.runAction(redClose.getDrive().actionBuilder(new Pose2d(-51, 49.5, Math.toRadians(307)))
+                .lineToX(-48)
+                .waitSeconds(1)
+                .turnTo(0)
+                .waitSeconds(2)
+                .lineToX(-25)
+                .build());
+
+        blueClose.runAction(blueClose.getDrive().actionBuilder(new Pose2d(-51, -49.5, Math.toRadians(53)))
+                .lineToX(-48)
+                .waitSeconds(1)
+                .turnTo(0)
+                .waitSeconds(2)
+                .lineToX(-25)
+                .build());
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_JUICE_BLACK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(redFar)
+                .addEntity(blueFar)
+                .addEntity(redClose)
+                .addEntity(blueClose)
+                .start();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAuto/BlueFar.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.BasicAuto;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+@Autonomous
+public class BlueFar extends LinearOpMode {
+    public void runOpMode() {
+        Pose2d initialPose = new Pose2d(62, 15, Math.PI);
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+
+        TrajectoryActionBuilder action = drive.actionBuilder(initialPose)
+                .lineToX(38);
+
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        Actions.runBlocking(
+                new SequentialAction(
+                        action.build()
+                )
+        );
+    }
+}
