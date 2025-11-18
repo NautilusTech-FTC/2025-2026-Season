a9dabb7e19342836341a07a35655719f984058bb
hnagd1 <hudsongraylaw@gmail.com>
2025-11-17 17:47:32 -0800

    started making auto, I'm switching computers

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAuto/ShootPreloadBlueFar.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.BasicAuto;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+public class ShootPreloadBlueFar extends LinearOpMode {
+    public void runOpMode () {
+        Pose2d initialPose = new Pose2d(62, -15, Math.PI);
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+
+        TrajectoryActionBuilder aim = drive.actionBuilder(initialPose)
+                .lineToX(0);
+    }
+}
