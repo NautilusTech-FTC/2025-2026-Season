058f52eca86c5e93c7b70f5b9df7587632c4c62a
YEtestacc <478967@bsd48.org>
2026-02-21 09:48:22 -0800

    Reverted to the external IMU

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Methods/VisionMethods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Methods/VisionMethods.java
@@ -86,7 +86,7 @@ public class VisionMethods {
 
         visionPortal = builder.build();
 
-        imu = hardwareMap.get(IMU.class, "imu");
+        imu = hardwareMap.get(IMU.class, "externalIMU");
         imu.initialize(new IMU.Parameters(new RevHubOrientationOnRobot(
                 RevHubOrientationOnRobot.LogoFacingDirection.FORWARD,
                 RevHubOrientationOnRobot.UsbFacingDirection.LEFT
